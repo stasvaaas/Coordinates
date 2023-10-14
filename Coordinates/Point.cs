e2d0f8f6a1f328c50b1fabdeda6776b02ba08bad
stasvaaas <105922844+stasvaaas@users.noreply.github.com>
2023-10-14 16:35:21 +0300

    Add project files.

---
+++ Coordinates/Point.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Coordinates
+{
+    public class Point
+    {
+        //counting Point instances
+        public static int PointCount { get; private set; }
+        public int X { get; private set; }
+        public int Y { get; private set; }
+
+        public Point(int x, int y)
+        {
+            X = x;
+            Y = y;
+            PointCount++;
+        }
+        
+        //get distance between 2 points
+        public static int GetDistanceBetweenPoints(Point start, Point end)
+        {
+            int AC = end.X - start.X;
+            int BC = end.Y - start.Y;
+            return (int)Math.Sqrt(AC * AC + BC * BC);
+        }
+
+        //get distance beetwenn current point and any other point
+        public int GetDistanceToPoint(Point point)
+        {
+            int AC = point.X - X;
+            int BC = point.Y - Y;
+            return (int)Math.Sqrt(AC * AC + BC * BC);
+        }
+    }
+}
---
+++ Coordinates/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Coordinates
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Point t = new Point(345, 100);
+            Point a = new Point(112, 30);
+            Point f = new Point(330, 240);
+            Console.WriteLine(t.GetDistanceToPoint(a));
+            Console.WriteLine(Point.GetDistanceBetweenPoints(a, f));
+            Console.WriteLine(Point.GetDistanceBetweenPoints(t, f));
+            Console.WriteLine(Point.PointCount);
+        }
+    }
+}
